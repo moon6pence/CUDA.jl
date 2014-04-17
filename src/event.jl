eb143ad644bab1f549d30cce147687bde8fd37e6
khkim <moon6pence@gmail.com>
2014-04-17 12:54:10 +0900

    Make CuEvent to be immutable object holding handle rather than typealias

--- src/event.jl
+++ src/event.jl
@@ -1,24 +1,26 @@
 # CUDA event
 
-typealias CuEvent lib.CUevent
+immutable CuEvent
+	handle::lib.CUevent
+end
 
 function create_event()
-	ref = Array(CuEvent, 1)
+	ref = Array(lib.CUevent, 1)
 	lib.cuEventCreate(pointer(ref), convert(Uint32, 0))
-	return ref[1]
+	return CuEvent(ref[1])
 end
 
 function destroy(event::CuEvent)
-	lib.cuEventDestroy_v2(event)
+	lib.cuEventDestroy_v2(event.handle)
 end
 
 function record(event::CuEvent)
 	# TODO: support stream
-	lib.cuEventRecord(event, convert(lib.CUstream, 0))
+	lib.cuEventRecord(event.handle, convert(lib.CUstream, 0))
 end
 
 function elapsed_time(start_event::CuEvent, end_event::CuEvent)
 	elapsed = Array(Float32, 1)
-	lib.cuEventElapsedTime(pointer(elapsed), start_event, end_event)
+	lib.cuEventElapsedTime(pointer(elapsed), start_event.handle, end_event.handle)
 	return elapsed[1]
 end
